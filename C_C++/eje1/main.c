b63588f2650ca079d92c899d5814285a95dce9ce
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2026-08-21 09:54:44 -0500

    Adicionando el primer ejemplo en C.

---
+++ C_C++/eje1/main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(){
+  int a = 0;
+  long b = 0;
+  float c = 0.;
+  double d = 0.;
+  
+  d = 3.*6.+5.;
+  printf("El resultado es: %f\n\r", d);
+  printf("Y su tamaño es: %ld\n\r", sizeof(a));
+  printf("Y su tamaño es: %ld\n\r", sizeof(b));
+  printf("Y su tamaño es: %ld\n\r", sizeof(c));
+  printf("Y su tamaño es: %ld\n\r", sizeof(d));
+
+  return 0;
+}
